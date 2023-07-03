3802faaca191e7a7041834d094394581cc4817d2
tuanacetinkaya <tuanacetinkaya7@gmail.com>
2023-07-04 00:05:32 +0300

    about section with animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">Tuana &nbsp; <span className="sm:block hidden"> | FullStack Developer</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Tuana &nbsp; <span className="sm:block hidden"> | Software Developer</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
